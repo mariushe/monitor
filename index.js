d7b94cd21c4dc1c91464deec2441ce8c068e43d9
Marius Herring <mariusherring@gmail.com>
2013-10-13 19:47:07 +0200

    Finished creating html, js, css structure

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var requestHandler = require("./requestHandler");
 
 var handle = {}
 
-handle["/"] = requestHandler.getData;
+
+handle["/"] = requestHandler.getMonitor;
+handle["/get-data"] = requestHandler.getData;
+handle["/assets/js/application.js"] = requestHandler.application;
+handle["/assets/css/application.css"] = requestHandler.css;
+handle["/assets/css/bootstrap.min.css"] = requestHandler.bootstrap;
 
 server.startApplication(router.route, handle);
--- requestHandler.js
+++ requestHandler.js
@@ -11,6 +11,23 @@ function application(query, response) {
     response.end();
 }
 
+function bootstrap(query, response) {
+    var body = fs.readFileSync("assets/css/bootstrap.min.css");
+
+    response.writeHead(200, {"Content-Type":"text/css"})
+    response.write(body);
+    response.end();
+}
+
+function css(query, response) {
+    var body = fs.readFileSync("assets/css/application.css");
+
+    response.writeHead(200, {"Content-Type":"text/css"})
+    response.write(body);
+    response.end();
+}
+
+
 function getData(query, response) {
     var body = fs.readFileSync("settings/status.json");
 
@@ -19,5 +36,18 @@ function getData(query, response) {
     response.end();
 }
 
+function getMonitor(query, response) {
+    var body = fs.readFileSync("assets/monitor.html");
+
+    response.writeHead(200, {"Content-Type":"text/html"});
+    response.write(body);
+    response.end();
+}
+
+
 exports.application = application;
 exports.getData = getData;
+exports.getMonitor = getMonitor;
+exports.bootstrap = bootstrap;
+exports.application = application;
+exports.css = css;
